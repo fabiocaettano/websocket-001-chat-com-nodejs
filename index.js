43c1d740718ef14a532e10c717d73042270df0f0
fabiocaettano <fabio.caettano74@gmail.com>
2025-02-14 21:32:59 +0000

    inserido a m o evento 'message' ra captar o que foi informadomado na página index.html

--- index.js
+++ index.js
@@ -11,13 +11,18 @@ app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'index.html'));
 });
 
-io.on('connection', (socket) => {
+io.on('connection', (socket) => {  
+
   console.log('a user connected');
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
+  
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg);
   });
-})
+  
+});
+
+
 
 server.listen(3000, () => {
-  console.log('server running at http://localhost:3000');
+  console.log('server running at port 3000');
 });
